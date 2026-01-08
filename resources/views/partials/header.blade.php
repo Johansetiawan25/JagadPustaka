7b6fc0705086e95a88e6f7422356b1697616422b
Johan Setiawan <johanreadyforsuccess@gmail.com>
2026-01-08 19:36:07 +0700

    iki bos

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,19 +12,39 @@
 
             <div class="col-md-3 text-end">
                 @if(Auth::check())
-                    <!-- Jika sudah login -->
-                    
-                    <form method="POST" action="{{ route('logout') }}" style="display:inline">
-                        @csrf
-                        <button type="submit" class="btn btn-light btn-sm">Logout</button>
-                    </form>
+                <!-- DROPDOWN USER -->
+                <div class="dropdown">
+                    <button class="btn btn-dark dropdown-toggle d-flex align-items-center"
+                        type="button"
+                        id="dropdownUser"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false">
+
+                        <i class="bi bi-person-circle fs-4 me-2"></i>
+                        Selamat Datang,
+                        <b class="ms-1">{{ Auth::user()->name }}</b>
+                    </button>
+
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser">
+                        <li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="dropdown-item text-danger fw-semibold">
+                                    <i class="bi bi-box-arrow-right me-2"></i> Logout
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
                 @else
-                    <!-- Jika belum login -->
-                    <a href="#" class="btn btn-light btn-sm">Daftar</a>
-                    <a href="{{ url('/login') }}" class="btn btn-light btn-sm">Login</a>
+                <!-- JIKA BELUM LOGIN -->
+                <a href="{{ route('register') }}" class="btn btn-light btn-sm me-1">Daftar</a>
+                <a href="{{ route('login') }}" class="btn btn-light btn-sm">Login</a>
                 @endif
             </div>
 
         </div>
+
+    </div>
     </div>
 </header>
