6c21efddd306daaa5364f03a01c43d2beb84f07a
Johan Setiawan <johanreadyforsuccess@gmail.com>
2026-01-08 16:02:59 +0700

    kasih login

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,16 +11,9 @@
             </div>
 
             <div class="col-md-3 text-end">
-                <a href="#" class="btn btn-light btn-sm">Masuk</a>
+                <a href="#" class="btn btn-light btn-sm">Daftar</a>
+                 <a href="/login" class="btn btn-light btn-sm">Login</a>
             </div>
-            
-             <!-- Login -->
-            <div class="col-md-3 text-end">
-                <a href="/login" class="btn btn-outline-primary btn-sm">
-                    Login
-                </a>
-            </div>
-
         </div>
     </div>
 </header>
